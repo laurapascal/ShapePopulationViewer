53bf67c4bd9113202269d8f163704733cf6fa839
Juan Carlos Prieto <juanprietob@gmail.com>
2015-11-23 14:24:32 -0500

    ENH: Unique color per direction
    Example for unique color per direction.

--- src/test/hsv.cxx
+++ src/test/hsv.cxx
@@ -60,7 +60,7 @@ int main()
     for(int i = 0; i  < numPts; i++){
         double normal[3];
         normalDataDouble->GetTuple(i, normal);
-        magnitud = std::max(magnitud, std::sqrt(normal[0]*normal[0] + normal[1]*normal[1] + normal[2]*normal[2]));
+        magnitud = std::max(magnitud, vtkMath::Normalize(normal));
     }
 
     /// Color map by direction with hsv color        
@@ -69,38 +69,78 @@ int main()
     scalars->SetNumberOfTuples(numPts);
     scalars->SetName("ColorByDirection");
 
-    for(int i = 0; i  < numPts; i++){
-        double normal[3];
-        normalDataDouble->GetTuple(i, normal);
+    bool hsv = false;
 
-        float HSV[3];
-        //MAGNITUD OF THE CURRENT VECTOR
-        double mag = std::sqrt(normal[0]*normal[0] + normal[1]*normal[1] + normal[2]*normal[2]);            
-        
-        //NORMALIZE THE VECTOR
-        vtkMath::Normalize(normal);
-        //HUE IS THE ANGLE OF THE VECTOR PROJECTED ONTO THE XY PLANE, 
-        HSV[0] =  (vtkMath::Pi() + std::atan2(normal[1],normal[0]))/(2.0*vtkMath::Pi());
-        //SATURATION IS THE VALUE OF THE ELEVATION ANGLE !! THE NORMAL OF THIS VECTOR IS = 1, RESCALE THE VALUE BETWEEN 0-1
-        //INVERT THE SATURATION SO WE HAVE WHITE AT THE POLES
-        HSV[1] = 1 - fabs((std::asin(normal[2]))/(vtkMath::Pi()/2.0));
-        //VALUE IS THE VALUE OF THE MAGNITUD AND IT HAS TO BE NORMALIZED WITH THE LARGEST VALUE OF THE VECTORS
-        HSV[2] = mag/magnitud;
+    if(hsv){
+        for(int i = 0; i  < numPts; i++){
+            double normal[3];
+            normalDataDouble->GetTuple(i, normal);
+
+            float HSV[3];
+            //MAGNITUD OF THE CURRENT VECTOR AND NORMALIZE
+            float mag = vtkMath::Normalize(normal);
+
+            //HUE IS THE ANGLE OF THE VECTOR PROJECTED ONTO THE XY PLANE, 
+            HSV[0] =  (vtkMath::Pi() + std::atan2(normal[1],normal[0]))/(2.0*vtkMath::Pi());
+            //SATURATION IS THE VALUE OF THE ELEVATION ANGLE !! THE NORMAL OF THIS VECTOR IS = 1, RESCALE THE VALUE BETWEEN 0-1
+            //INVERT THE SATURATION SO WE HAVE WHITE AT THE POLES
+            HSV[1] = 1 - fabs((std::asin(normal[2]))/(vtkMath::Pi()/2.0));
+            //VALUE IS THE VALUE OF THE MAGNITUD AND IT HAS TO BE NORMALIZED WITH THE LARGEST VALUE OF THE VECTORS
+            HSV[2] = mag/magnitud;
 
 
-        float RGB[3];
-        vtkMath::HSVToRGB(HSV, RGB);
-        vtkMath::MultiplyScalar(RGB, 255);
+            float RGB[3];
+            vtkMath::HSVToRGB(HSV, RGB);
+            vtkMath::MultiplyScalar(RGB, 255);
 
-        scalars->InsertTuple3 (i, (unsigned char)RGB[0], (unsigned char)RGB[1], (unsigned char)RGB[2]);
-        
-        // vtkMath::MultiplyScalar(normal, 1.0/magnitud);
-        // unsigned char r = abs(255*normal[0]);
-        // unsigned char g = abs(255*normal[1]);
-        // unsigned char b = abs(255*normal[2]);
-        // scalars->InsertTuple3 (i, r, g, b);
+            scalars->InsertTuple3 (i, (unsigned char)RGB[0], (unsigned char)RGB[1], (unsigned char)RGB[2]);
+            
+            // vtkMath::MultiplyScalar(normal, 1.0/magnitud);
+            // unsigned char r = abs(255*normal[0]);
+            // unsigned char g = abs(255*normal[1]);
+            // unsigned char b = abs(255*normal[2]);
+            // scalars->InsertTuple3 (i, r, g, b);
 
+        }
+
+    }else{
+
+        for(int i = 0; i  < numPts; i++){
+            double normal[3];
+            normalDataDouble->GetTuple(i, normal);
+
+            //NORMALIZE THE VECTOR, CALCULATE THE COEFFICIENT
+            float magcoeff = vtkMath::Normalize(normal)/magnitud;
+            
+
+            float RGB[3];
+            float r = (1.0 + normal[0])/2.0;
+            float g = (1.0 + normal[1])/2.0;
+            float b = (1.0 + normal[2])/2.0;
+
+            //CALCULATE THE WEIGHTS FOR THE COLORING
+            float fn[3];
+            fn[0] = fabs(normal[0]);
+            fn[1] = fabs(normal[1]);
+            fn[2] = fabs(normal[2]);
+            float w = fn[0] + fn[1] + fn[2];
+
+            //USE THE WEIGHTS AND COMPUTE THE CONTRIBUTION FOR EACH COMPONENT RGB. 
+
+            //[ r   1-g 1-b        [fn[0]
+            //  1-r g   1-b    X    fn[1]   X  255/(w*magcoeff)
+            //  1-r 1-g b]          fn[2]]
+
+            RGB[0] = (r*fn[0] + (1 - g)*fn[1] + (1 - b)*fn[2])*255/w*magcoeff;
+            RGB[1] = ((1 - r)*fn[0] + g*fn[1] + (1 - b)*fn[2])*255/w*magcoeff;
+            RGB[2] = ((1 - r)*fn[0] + (1 - g)*fn[1] + b*fn[2])*255/w*magcoeff;
+
+            scalars->InsertTuple3 (i, (unsigned char)RGB[0], (unsigned char)RGB[1], (unsigned char)RGB[2]);
+
+        }
     }
+
+    
     
     
     polyData->GetPointData()->AddArray(scalars);
